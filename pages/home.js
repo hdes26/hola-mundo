0b8577fe6c826b4c1bcc1c9a45ae8c12d30a1d0b
John Doe <adaptingear@gmail.com>
2025-02-15 13:42:45 -0500

    what whats happend

--- pages/home.js
+++ pages/home.js
@@ -1,21 +1,6 @@
 export default function Home() {
     return `
-        <div class="container">
-            <div class="form-wrapper">
-                <h1>Completa tus datos para obtener tu cupón</h1>
-                <form id="couponForm">
-                    <label for="nombre">Nombre</label>
-                    <input type="text" id="nombre" name="nombre" required />
-
-                    <label for="correo">Correo electrónico</label>
-                    <input type="email" id="correo" name="correo" required />
-
-                    <label for="telefono">Número de teléfono</label>
-                    <input type="tel" id="telefono" name="telefono" required />
-
-                    <button type="submit" class="submit-button">Recibir mi cupón</button>
-                </form>
-            </div>
-        </div>
+        <h1>Bienvenido a mi SPA V2</h1>
+        <a href="/about" data-link>Ir a About</a>
     `;
 }
