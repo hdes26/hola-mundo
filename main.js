bac3731d5f58fafc7ab4e192e5990cd6c155565f
John Doe <adaptingear@gmail.com>
2025-02-15 13:46:43 -0500

    test

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const { initRouter } = require("./router.js");
 (() => {
     // Crear la estructura base del HTML
     document.body.innerHTML = `
-        <div id="app"></div>
+        <div id="app">HOLA</div>
     `;
 
 
